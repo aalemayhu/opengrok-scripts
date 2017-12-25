aa2de1e63a67142cba404fabd7f8a6fe5067a693
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-25 17:56:06 +0100

    add script for synching repositories
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ repositories.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REPOSITORIES=(
+https://github.com/cilium/cilium
+git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git
+git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf.git
+git://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git
+git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+)
+
+for REPO in ${REPOSITORIES[@]}; do
+  DIR=$HOME/src/`basename $REPO .git`
+  if test -d $DIR; then
+    git -C $DIR fetch origin master
+  else
+    git clone $REPO $DIR
+  fi
+  git -C $DIR reset --hard origin master
+done
