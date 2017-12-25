0c6d431cc1ec7cba5cfda8517db103b40aacdb4b
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-25 18:18:18 +0100

    use home directory for opengrok files
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- run-opengrok.sh
+++ run-opengrok.sh
@@ -9,6 +9,6 @@ docker run --net cilium-net \
   -p 8080:8080 \
   -l "app=opengrok" \
   --name opengrok -d --rm \
-  -v `pwd`/src:/src \
-  -v `pwd`/opengrok_data:/data \
+  -v $HOME/src:/src \
+  -v $HOME/opengrok_data:/data \
   scue/docker-opengrok
